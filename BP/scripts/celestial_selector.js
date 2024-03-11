7fdfe69679ff54cf9b9532db0c28a191e8a34760
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-03-11 03:17:38 +0200

    celestial selector v0.0.2
    JSON UI is PAIN

--- BP/scripts/celestial_selector.js
+++ BP/scripts/celestial_selector.js
@@ -1,50 +1,64 @@
 import { ActionFormData} from "@minecraft/server-ui" ;
 import { world, system } from "@minecraft/server" ;
 
-//This file is still Work in progress
-
 const overworld = world.getDimension('overworld');
+const rocket_tier = 3;
 
-function open_form(player) {
-	let Form = new ActionFormData()
+const solar_system = {
+	Sol: {tier: 100, x:200, y:101},
+	Mercury: {tier: 4, x:163, y:80},
+	Venus: {tier: 3, x:137, y:124},
+	Overworld: {tier: 1, x:280, y:130},
+	Mars: {tier: 2, x:246, y:46},
+	Asteroids: {tier: 3, x:72, y:107},
+	Jupiter: {tier: 4, x:290, y:156},
+	Saturn: {tier: 100, x:106, y:167},
+	Uranus: {tier: 100, x:174, y:10},
+	Neptune: {tier: 100, x:19, y:145},
+}
+
+function zoom_at(player, planet) {
+	world.sendMessage("zoom at "+ planet)
+}
+
+function select_solar_system(player, focused, tier) {
+	let form = new ActionFormData()
 	.title("Celestial Panel Sol")
-	.body("Click again to zoom (view moons and satellites)")
-	.button("§fx200y101Sol")
-	.button("§fx163y80Mercury")
-	.button("§fx137y124Venus")
-	.button("§tx280y130Overworld")
-	.button("§fx246y46Mars")
-	.button("§tx72y107Asteroids")
-	.button("§fx290y156Jupiter")
-	.button("§fx106y167Saturn")
-	.button("§fx174y10Uranus")
-	.button("§fx19y145Neptune")
-	.button("LAUNCH")
+	if (focused != '') {
+		form.body(
+			`§${tier >= solar_system[focused].tier ? 't' : 'f'}`+
+			`Tier ${solar_system[focused].tier < 6 ? '' + solar_system[focused]?.tier : '?' }`+
+			`${focused}`
+		)
+	}
+	for (let planet of Object.keys(solar_system)) {
+		form.button(
+			`§${tier >= solar_system[planet].tier ? 't' : 'f'}`+
+			`§${focused == planet ? 't' : 'f'}`+
+			`x${solar_system[planet].x}`+
+			`y${solar_system[planet].y}`+
+			`${planet}`
+		)
+	}
+	form.button("LAUNCH")
 	.button("CREATE")
 	.show(player)
 	.then((response) => {
 		if (response.canceled) {
-			world.sendMessage("back")//open_form(player)
+			world.sendMessage("back"); return;
 		}
 		switch (response.selection) {
-			case 0: world.sendMessage("sun"); break;
-			case 1: world.sendMessage("mercury"); break;
-			case 2: world.sendMessage("venus"); break;
-			case 3: world.sendMessage("overwoeld"); break;
-			case 4: world.sendMessage("mars"); break;
-			case 5: world.sendMessage("asteroids"); break;
-			case 6: world.sendMessage("jupiter"); break;
-			case 7: world.sendMessage("saturn"); break;
-			case 8: world.sendMessage("uranus"); break;
-			case 9: world.sendMessage("neptune"); break;
-			case 10: world.sendMessage("go"); break;
-			case 11: world.sendMessage("build space station"); break;
+			case 10: world.sendMessage("go"); return; break;
+			case 11: world.sendMessage("build space station"); return; break;
 		}
+		const planet = Object.keys(solar_system)[response.selection]
+		if (planet == focused) {zoom_at(player, planet)}
+		else {select_solar_system(player, planet, rocket_tier)}
 	})
 }
 
 world.afterEvents.itemUse.subscribe(({itemStack, source}) => {
 	if ( (itemStack.typeId === "minecraft:compass") ) {
-		open_form(source)
+		select_solar_system(source, '', rocket_tier)
 	}
 })

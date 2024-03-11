2d8d716229e32ac8fa366f81d349865eb69b3aee
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-03-11 17:57:14 +0200

    celestial selector v0.0.3
    Added the system to support moons to the catalog
    Made all the bindings into variables.

--- BP/scripts/celestial_selector.js
+++ BP/scripts/celestial_selector.js
@@ -16,24 +16,80 @@ const solar_system = {
 	Uranus: {tier: 100, x:174, y:10},
 	Neptune: {tier: 100, x:19, y:145},
 }
-
-function zoom_at(player, planet) {
-	world.sendMessage("zoom at "+ planet)
+const moons = {
+	Sol: {},
+	Mercury: {},
+	Venus: {},
+	Overworld: {Moon: {tier: 1, x:280, y:130}},
+	Mars: {},
+	Asteroids: {},
+	Jupiter: {},
+	Saturn: {},
+	Uranus: {},
+	Neptune: {},
 }
 
-function select_solar_system(player, focused, tier) {
+function zoom_at(player, focused, planet) {
 	let form = new ActionFormData()
-	.title("Celestial Panel Sol")
+	.title("Celestial Panel " + planet)
+	if (focused == planet) {
+		form.body(
+			`§${rocket_tier >= solar_system[planet].tier ? 't' : 'f'}`+
+			`Tier ${solar_system[planet].tier < 6 ? '' + solar_system[focused]?.tier : '?' }`+
+			`${planet}`
+		)
+	} else {
+		world.sendMessage(JSON.stringify(moons[planet][focused].tier))
+		form.body(
+			`§${rocket_tier >= moons[planet][focused].tier ? 't' : 'f'}`+
+			`Tier ${moons[planet][focused].tier < 6 ? '' + moons[planet][focused]?.tier : '?' }`+
+			`${focused}`
+		)
+	}
+	form.button(
+		`§${rocket_tier >= solar_system[planet].tier ? 't' : 'f'}`+
+		`§${focused == planet ? 't' : 'f'}`+
+		`${planet}`
+	)
+	for (let moon of Object.keys(moons[planet])) {
+		form.button(
+			`§${rocket_tier >= moons[planet][moon].tier ? 't' : 'f'}`+
+			`§${focused == moon ? 't' : 'f'}`+
+			`x${moons[planet][moon].x}`+
+			`y${moons[planet][moon].y}`+
+			`${moon}`
+		)
+	}
+	form.button("LAUNCH")
+	.button("CREATE")
+	.show(player)
+	.then((response) => {
+		if (response.canceled) {
+			select_solar_system(player, ''); return;
+		}
+		switch (response.selection) {
+			case 0: zoom_at(player, planet, planet); return; break;
+			case Object.keys(moons[planet]).length + 1: world.sendMessage("go"); return; break;
+			case Object.keys(moons[planet]).length + 2: world.sendMessage("build space station"); return; break;
+		}
+		const moon = Object.keys(moons[planet])[response.selection - 1];
+		zoom_at(player, moon, planet);
+	})
+}
+
+function select_solar_system(player, focused) {
+	let form = new ActionFormData()
+	.title("Celestial Panel Solar System")
 	if (focused != '') {
 		form.body(
-			`§${tier >= solar_system[focused].tier ? 't' : 'f'}`+
+			`§${rocket_tier >= solar_system[focused].tier ? 't' : 'f'}`+
 			`Tier ${solar_system[focused].tier < 6 ? '' + solar_system[focused]?.tier : '?' }`+
 			`${focused}`
 		)
 	}
 	for (let planet of Object.keys(solar_system)) {
 		form.button(
-			`§${tier >= solar_system[planet].tier ? 't' : 'f'}`+
+			`§${rocket_tier >= solar_system[planet].tier ? 't' : 'f'}`+
 			`§${focused == planet ? 't' : 'f'}`+
 			`x${solar_system[planet].x}`+
 			`y${solar_system[planet].y}`+
@@ -52,13 +108,13 @@ function select_solar_system(player, focused, tier) {
 			case 11: world.sendMessage("build space station"); return; break;
 		}
 		const planet = Object.keys(solar_system)[response.selection]
-		if (planet == focused) {zoom_at(player, planet)}
-		else {select_solar_system(player, planet, rocket_tier)}
+		if (planet == focused) {zoom_at(player, planet, planet)}
+		else {select_solar_system(player, planet)}
 	})
 }
 
 world.afterEvents.itemUse.subscribe(({itemStack, source}) => {
 	if ( (itemStack.typeId === "minecraft:compass") ) {
-		select_solar_system(source, '', rocket_tier)
+		select_solar_system(source, '')
 	}
 })

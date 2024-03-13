ca8bed169bd21986b27afa06f74f77fed49c16e6
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-03-13 22:25:52 +0200

    Celestial Selector v0.0.3

--- BP/scripts/celestial_selector.js
+++ BP/scripts/celestial_selector.js
@@ -4,23 +4,58 @@ import { world, system } from "@minecraft/server" ;
 const overworld = world.getDimension('overworld');
 const rocket_tier = 3;
 
-const solar_system = {
-	Sol: {tier: 100, x:200, y:101},
-	Mercury: {tier: 4, x:163, y:80},
-	Venus: {tier: 3, x:137, y:124},
-	Overworld: {tier: 1, x:280, y:130},
-	Mars: {tier: 2, x:246, y:46},
-	Asteroids: {tier: 3, x:72, y:107},
-	Jupiter: {tier: 4, x:290, y:156},
-	Saturn: {tier: 100, x:106, y:167},
-	Uranus: {tier: 100, x:174, y:10},
-	Neptune: {tier: 100, x:19, y:145},
+function set_planet_locations() {
+	const middle = {x:200, y:101};
+	for (let planet of Object.keys(solar_system)) {
+		if (planet == "Asteroids" || planet == "Sol") continue;
+		const tick = system.currentTick/20;
+		const speed = -360/solar_system[planet].turn
+		const angle = (tick * speed - solar_system[planet].init_angle) % 360
+		const coods = angle_to_vector(angle, solar_system[planet].distance, middle);
+		solar_system[planet].x = coods.x
+		solar_system[planet].y = coods.y
+	}
 }
+function set_astroids_locations() {
+	const middle = {x:200, y:101};
+	const random_angle = Math.random() * 360;
+	const coods = angle_to_vector(random_angle, solar_system.Asteroids.distance, middle);
+	solar_system.Asteroids.x = coods.x
+	solar_system.Asteroids.y = coods.y
+}
+function set_moon_locations() {
+	const middle = {x:161, y:81};
+	const time = 3 * (world.getTimeOfDay() + 12000) / 200;
+	const coods = angle_to_vector(-time, moons.Overworld.Moon.distance, middle);
+	moons.Overworld.Moon.x = coods.x
+	moons.Overworld.Moon.y = coods.y
+}
+function angle_to_vector(angle, distance, center) {
+	angle = (Math.PI / 180) * angle; // to randian
+	return {
+		x: Math.round(distance * Math.cos(angle)) + center.x,
+		y: Math.round(distance * Math.sin(angle)/2) + center.y,
+	}
+}
+
+const solar_system = {
+	Sol: {tier: 100, x: 200, y: 101},
+	Mercury: {tier: 4, distance: 56, turn: 88, init_angle: 250},
+	Venus: {tier: 3, distance: 78, turn: 225, init_angle: 178},
+	Overworld: {tier: 1, distance: 98, turn: 360, init_angle: 100},
+	Mars: {tier: 2, x:246, distance: 119, turn: 687, init_angle: 0},
+	Asteroids: {tier: 3, distance: 126},
+	Jupiter: {tier: 4, distance: 141, turn: 11.86 * 360, init_angle: 36},
+	Saturn: {tier: 100, distance: 161, turn: 29.46 * 360, init_angle: 47},
+	Uranus: {tier: 100, distance: 182, turn: 84.01 * 360, init_angle: 315},
+	Neptune: {tier: 100, distance: 200, turn: 164.79 * 360, init_angle: 296},
+}
+
 const moons = {
 	Sol: {},
 	Mercury: {},
 	Venus: {},
-	Overworld: {Moon: {tier: 1, x:274, y:24}},
+	Overworld: {Moon: {tier: 1, distance:161}},
 	Mars: {},
 	Asteroids: {},
 	Jupiter: {},
@@ -51,6 +86,7 @@ function zoom_at(player, focused, planet) {
 		`${planet}`
 	)
 	for (let moon of Object.keys(moons[planet])) {
+		set_moon_locations()
 		form.button(
 			`§${rocket_tier >= moons[planet][moon].tier ? 't' : 'f'}`+
 			`§${focused == moon ? 't' : 'f'}`+
@@ -77,6 +113,9 @@ function zoom_at(player, focused, planet) {
 }
 
 function select_solar_system(player, focused) {
+	
+	set_planet_locations()
+	
 	let form = new ActionFormData()
 	.title("Celestial Panel Solar System")
 	if (focused != '') {
@@ -118,6 +157,7 @@ function launch(player, planet) {
 
 world.afterEvents.itemUse.subscribe(({itemStack, source}) => {
 	if ( (itemStack.typeId === "minecraft:compass") ) {
+		set_astroids_locations()
 		select_solar_system(source, '')
 	}
 })
